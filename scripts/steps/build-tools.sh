97c7edd4e2afc0708ab322e74d7344f835074be0
Timothée EID <timothee.eid@erizo.fr>
2015-07-07 13:44:37 +0200

    Fix IMAP provision scripts

--- scripts/steps/build-tools.sh
+++ scripts/steps/build-tools.sh
@@ -4,6 +4,7 @@ echo ""
 echo "--------------------------------------------------"
 echo "Installing build tools ..."
 echo "--------------------------------------------------"
+apt-get install -y vim
 apt-get install -y git build-essential automake
 apt-get install -y curl
 apt-get install -y unzip
--- scripts/steps/imap.sh
+++ scripts/steps/imap.sh
@@ -13,13 +13,16 @@ command -v dovecot >/dev/null 2>&1 || {
 echo "Configuring ..."
 
 # Use maildir
-sed -i 's/^mail_location.*$/mail_location = maildir:~\/Maildir/' /etc/dovecot/conf.d/10-mail.conf
+sed -i 's/^#mail_location.*$/mail_location = maildir:~\/Maildir/' /etc/dovecot/conf.d/10-mail.conf
 
 # Strip domain from auth attempts
 sed -i 's/^.*auth_username_format.*$/auth_username_format = %Ln/' /etc/dovecot/conf.d/10-auth.conf
 
 # Allow unsecured auth
 sed -i 's/^#disable_plaintext_auth.*$/disable_plaintext_auth = no/' /etc/dovecot/conf.d/10-auth.conf
+
+# Add postmaster address
+sed -i 's/protocol lmtp {/protocol lmtp {\n  postmaster_address = root@localhost/' 20-lmtp.conf
 
 echo "Reload configuration ..."
 /etc/init.d/dovecot restart
